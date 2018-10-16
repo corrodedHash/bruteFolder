fcd20e0f2e6d389aa03a570e4c86485cbc783e13
corrodedHash <corrodedHash@users.noreply.github.com>
2018-10-16 19:51:32 +0200

    Refactored

--- brutefolder/chain.py
+++ brutefolder/chain.py
@@ -1,6 +1,6 @@
 """Contains Chain class"""
 
-from typing import List
+from typing import List, Tuple
 from .direction import turn, get_turn_diff
 
 
@@ -13,7 +13,7 @@ class Chain:
             self.whole_chain.append(turn(self.whole_chain[-1], direction))
 
         self.fold_list: List[int] = []
-        self.cut_up: List[int] = []
+        self.chain_history: List[Tuple[int, int]] = []
         self.chain_start = 0
         self.chain_end = len(self.whole_chain)
 
@@ -34,32 +34,30 @@ class Chain:
         last_part = self.chain[index + 1:]
         parts = zip(first_part, last_part)
 
-        def match(x, y):
+        def match(x: int, y: int) -> bool:
             return get_turn_diff(x, turn(y, turn_dir)) == 2
         return all(match(before, after) for before, after in parts)
 
     def fold(self, index: int) -> None:
         """Actually folds the chain. Does not check for validity"""
+        assert index + 1 < len(self)
+
         self.fold_list.append(index)
-        if 2 * index >= len(self) - 2:
-            self.cut_up.append(-self.chain_end)
-            self.chain_end = self.chain_start + index + 1
+        self.chain_history.append((self.chain_start, self.chain_end))
+
+        actual_index = self.chain_start + index + 1
+
+        if 2 * (index + 1) >= len(self):
+            self.chain_end = actual_index
         else:
-            self.cut_up.append(self.chain_start)
-            self.chain_start += index + 1
+            self.chain_start = actual_index
 
     def reset_fold(self) -> None:
         """Resets the last fold"""
         assert self.fold_list
-        assert self.cut_up
 
         self.fold_list.pop()
-        last_index = self.cut_up.pop()
-
-        if last_index < 0:
-            self.chain_end = -1 * last_index
-        else:
-            self.chain_start = last_index
+        self.chain_start, self.chain_end = self.chain_history.pop()
 
     def folded(self) -> bool:
         """Checks if the chain can be folded further"""
