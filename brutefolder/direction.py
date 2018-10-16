1d20e0f2a5f3b47e2379906e8f84efc7729f86fc
corrodedHash <corrodedHash@users.noreply.github.com>
2018-10-16 19:11:31 +0200

    Miniscule refactoring

--- brutefolder/direction.py
+++ brutefolder/direction.py
@@ -24,11 +24,9 @@ def get_turn_diff(start: int, end: int) -> int:
 def str_to_dir_list(inp: str)-> List[int]:
     """Return list with directions as given in input"""
     result = []
+    char_dir_map = {'r': 1, 'l': 3}
     for dir_char in inp.lower():
-        if dir_char == "r":
-            result.append(1)
-        elif dir_char == "l":
-            result.append(3)
-        else:
-            raise ValueError
+        assert dir_char in char_dir_map
+        result.append(char_dir_map[dir_char])
+
     return result
