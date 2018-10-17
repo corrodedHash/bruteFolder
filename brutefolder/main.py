32da5e459d6c9a637a0e91154c6996c0b9932787
corrodedHash <corrodedHash@users.noreply.github.com>
2018-10-17 14:35:51 +0200

    Made main output more human readable

--- brutefolder/main.py
+++ brutefolder/main.py
@@ -9,17 +9,17 @@ from . import chain
 
 def main() -> None:
     """Main function"""
-    search_chain = chain.Chain(direction.str_to_dir_list("LRRLLLRRLRRRLLR"))
+    #search_chain = chain.Chain(direction.str_to_dir_list("LRRLLLRRLRRRLLR"))
+    search_chain = chain.Chain(direction.str_to_dir_list("LLRRLLRR"))
     best_fold: List[int] = []
     best_fold_count = 0
     print("chain: " + str(search_chain.chain))
-    counter = 0
+    fold_count = 0
     for current_fold in folding.get_folds(search_chain):
-        counter += 1
+        fold_count += 1
 
-        if counter >= 2000:
+        if fold_count >= 2000:
             print("Checked " + str(counter))
-            counter = 0
 
         if not best_fold or len(current_fold) < len(best_fold):
             best_fold = copy.deepcopy(current_fold)
@@ -29,8 +29,9 @@ def main() -> None:
         elif len(current_fold) == len(best_fold):
             best_fold_count += 1
 
-    print(best_fold)
-    print(best_fold_count)
+    print("There are {0} ways to fold the sequence".format(fold_count) )
+    print("There are {0} optimal ways to fold the sequence".format(best_fold_count))
+    print("One of those is: {0}".format(best_fold))
 
 if __name__ == "__main__":
     main()
